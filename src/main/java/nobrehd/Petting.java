99800c938c4431c378a4d54b2bab099ff29d59eb
NobreHD <60514067+NobreHD@users.noreply.github.com>
2023-07-22 01:35:19 +0100

    Update to 1.20.1, changed API, rewrite event and message logic

--- src/main/java/nobrehd/Petting.java
+++ src/main/java/nobrehd/Petting.java
@@ -2,8 +2,10 @@ package nobrehd;
 
 import com.google.common.reflect.TypeToken;
 import com.google.gson.Gson;
-import net.md_5.bungee.api.ChatColor;
-import net.md_5.bungee.api.chat.TextComponent;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.text.TextReplacementConfig;
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Location;
 import org.bukkit.Particle;
 import org.bukkit.command.Command;
@@ -34,48 +36,32 @@ public final class Petting extends JavaPlugin implements Listener, CommandExecut
         this.getServer().getPluginManager().registerEvents(this, this);
         getLogger().info("Petting Listener Added");
         repeat_id = this.getServer().getScheduler().scheduleSyncRepeatingTask(this, () -> {
-            List keys = new ArrayList(timeout_players.keySet());
-            for (Object key: keys) {
-                long unix = (Long) timeout_players.get((String) key);
-                long actual_unix = System.currentTimeMillis()/1000L;
-                if (unix + 3 < actual_unix){
-                    timeout_players.remove((String) key);
-                }
+            // remove players from timeout
+            long actual_unix = System.currentTimeMillis()/1000L;
+            for (Object key : timeout_players.keySet()){
+                long unix = (Long) timeout_players.get(key);
+                if (unix + 3 < actual_unix) timeout_players.remove(key);
             }
         }, 0, 60);
         this.getCommand("update").setExecutor(this);
     }
 
     @Override
-    public void onDisable() {}
+    public void onDisable() {
+        this.getServer().getScheduler().cancelTask(repeat_id);
+        getLogger().info("Petting Listener Removed");
+    }
 
     @EventHandler
     public void PlayerRightClick(PlayerInteractEntityEvent event){
         Player player = event.getPlayer();
         if (player.isSneaking()) {
             Entity entity = event.getRightClicked();
-            String nametag = entity.getCustomName();
-            List<String> selection = new LinkedList<String>(Arrays.asList());
-            selection.addAll(this.greetings.get("both"));
-            if (entity instanceof Wolf){
-                if (nametag != null){
-                    selection.addAll(this.greetings.get("dogs_named"));
-                }
-                selection.addAll(this.greetings.get("dogs"));
-            }else if(entity instanceof Cat){
-                if (nametag != null){
-                    selection.addAll(this.greetings.get("cats_named"));
-                }
-                selection.addAll(this.greetings.get("cats_named"));
-            }else return;
-            String text = selection.get(rand.nextInt(selection.size())).replace("?name", nametag == null? "" : nametag);
-            Tameable cuties = (Tameable) entity;
-            if (!cuties.isTamed()) return;
-            Location spawn = cuties.getLocation();
-            spawn.add(0, 1, 0);
-            player.spawnParticle(Particle.VILLAGER_HAPPY, spawn, 2);
-            TextComponent message = new TextComponent("\u2661 " + text + " \u2661");
-            message.setColor(ChatColor.LIGHT_PURPLE);
+
+            // is the entity tamed?
+            if (!(entity instanceof Tameable)) return;
+
+            // is the player in time-out?
             String timeout_key = player.getUniqueId() + "-" + entity.getUniqueId();
             if (timeout_players.containsKey(timeout_key)){
                 long unix = (Long) timeout_players.get(timeout_key);
@@ -87,7 +73,31 @@ public final class Petting extends JavaPlugin implements Listener, CommandExecut
                 timeout_players.remove(timeout_key);
             }
             timeout_players.put(timeout_key, System.currentTimeMillis()/1000L);
-            player.sendMessage(message);
+
+            Component nametag = entity.customName();
+
+            List<String> selection = new LinkedList<String>(List.of());
+            if (entity instanceof Wolf){
+                if (nametag != null) selection.addAll(this.greetings.get("dogs_named"));
+                selection.addAll(this.greetings.get("dogs"));
+            }else if(entity instanceof Cat){
+                if (nametag != null) selection.addAll(this.greetings.get("cats_named"));
+                selection.addAll(this.greetings.get("cats_named"));
+            }else return;
+
+            TextReplacementConfig replacementConfig = TextReplacementConfig.builder()
+                    .match("{name}")
+                    .replacement(nametag != null ? nametag : Component.text(""))
+                    .build();
+            Component text = Component.text(selection.get(rand.nextInt(selection.size())))
+                    .replaceText(replacementConfig);
+
+            Component heart = Component.text(" ♡ ").color(NamedTextColor.RED);
+
+            Location spawn = entity.getLocation();
+            spawn.add(0, 1, 0);
+            player.spawnParticle(Particle.VILLAGER_HAPPY, spawn, 2);
+            player.sendMessage(heart.append(text).append(heart));
             event.setCancelled(true);
         }
     }
@@ -98,34 +108,37 @@ public final class Petting extends JavaPlugin implements Listener, CommandExecut
             Map<String, List<String>> list = update();
             if (list != null) {
                 this.greetings = list;
-                sender.sendMessage("List Updated");
-                return true;
-            }
-            sender.sendMessage("Error Receiving Data");
-        }else{
-            sender.sendMessage("You don't have permissions to run this command");
-        }
-        return false;
+                sendMessage(sender, "Data Updated", NamedTextColor.GREEN);
+            } else sendMessage(sender, "Error Updating Data", NamedTextColor.RED);
+        }else sendMessage(sender, "You don't have permission to use this command", NamedTextColor.RED);
+        return true;
     }
 
     static public Map<String, List<String>> update(){
         try {
-            URL url = new URL("https://nobrehd.github.io/Petting-Plugin/api/greetings.json");
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
-            connection.setRequestProperty("Content-Type", "application/json");
+            URL url = new URL("https://api.nobrehd.pt/petting");
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+            con.setRequestProperty("Accept", "application/json");
 
-            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            String inputLine;
-            StringBuffer content = new StringBuffer();
-            while ((inputLine = in.readLine()) != null) {
-                content.append(inputLine);
+            if (con.getResponseCode() != 200) {
+                return null;
             }
-            in.close();
-            connection.disconnect();
-            Type mapType = new TypeToken<Map<String,List<String>>>(){}.getType();
-            return new Gson().fromJson(content.toString(), mapType);
-        } catch (Exception e){}
+
+            BufferedReader br = new BufferedReader(new InputStreamReader((con.getInputStream())));
+            String output = br.readLine();
+            con.disconnect();
+            Gson gson = new Gson();
+            Type type = new TypeToken<Map<String, List<String>>>(){}.getType();
+            return gson.fromJson(output, type);
+        } catch (Exception ignored){}
         return null;
+    }
+
+    static void sendMessage(CommandSender sender, String message, NamedTextColor color){
+        TextComponent prefix = Component.text("[Petting] ")
+                .color(NamedTextColor.LIGHT_PURPLE)
+                .append(Component.text(message, color));
+        sender.sendMessage(prefix);
     }
 }
